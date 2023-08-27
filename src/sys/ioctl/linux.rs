3fd726031ec322d868b82dbe8a7cba96abbbb20c
Denis Drakhnia <numas13@gmail.com>
2023-08-27 17:50:23 +0300

    e2k

--- src/sys/ioctl/linux.rs
+++ src/sys/ioctl/linux.rs
@@ -42,7 +42,8 @@ mod consts {
     target_arch = "x86_64",
     target_arch = "aarch64",
     target_arch = "riscv32",
-    target_arch = "riscv64"
+    target_arch = "riscv64",
+    target_arch = "e2k64"
 ))]
 mod consts {
     #[doc(hidden)]
